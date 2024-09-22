5a64e431e6c91652f740d623ce1e36b9679f7476
Pamitha <89682916+Pamitha-Pankaja@users.noreply.github.com>
2024-09-22 08:57:28 +0530

    Created the product management function

---
+++ BackendServices/BackendServices/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using BackendServices.Models;
+
+namespace BackendServices;
+
+// Repositories/IProductRepository.cs
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllProducts();
+    Task<Product> GetProductById(string productId);
+    Task CreateProduct(Product product);
+    Task UpdateProduct(Product product);
+    Task DeleteProduct(string productId);
+    Task ActivateProduct(string productId);
+    Task DeactivateProduct(string productId);
+}
