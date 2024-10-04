b422343c6859f5cc62e7caaa01d99ae066fe0cf3
IT21786464 <97947640+TNjo@users.noreply.github.com>
2024-10-04 10:22:16 +0530

    Revert "Oder Management"

--- BackendServices/BackendServices/DTOs/OrderDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BackendServices.DTOs;
-
-public class OrderDTO
-{
-    public string CustomerId { get; set; }
-    public List<OrderItemDTO> Items { get; set; }
-    public string Note { get; set; } // For cancellation
-}
